a1c4ebef895fa9c63f7da6cfdbd63fc37045b4b6
eugeniy3339 <122208682+eugeniy3339@users.noreply.github.com>
2024-10-27 11:22:54 +0100

    0.0.1

---
+++ Assets/Scripts/WindowsManagerScript.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WindowsManagerScript : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> panels = new List<GameObject>();
+
+    public void OpenLayout(string name)
+    {
+        foreach (var panel in panels)
+        {
+            if (panel.name == name) panel.SetActive(true);
+            else panel.SetActive(false);
+        }
+    }
+
+    public void StartGame(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
